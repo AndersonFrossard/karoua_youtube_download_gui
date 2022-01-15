501a0e6a68f7efed9c983b8edfed5e880eb31b2d
AndersonFrossard <frossard2008@gmail.com>
2022-01-15 15:21:33 -0300

    Adding requiments.txt and file containing suggestions of next implementations.

--- youtube_download.py
+++ youtube_download.py
@@ -1,36 +1,4 @@
-"""
-Para criar o virtualEnv
-pip install virtualenv  (se nao tiver instalado ainda)
-cria o diretorio onde irá ficar o seu virtualenv. 
-p.ex. mkdir meuvirtualenv
-entra nesse diretorio p.ex. cd meuvirtualenv
-entao roda este comando
-virtualenv .
-
-(beleza, criou o virtualenv)
-
-Para fazer rodar no sublime:
-ir em Tools > Build System > New Build System
-daí voce cola este texto:
-
-{
-    "cmd": ["C:/@git/whatsapp_video.py/minhavenv/Scripts/python.exe", "-u", "$file"],
-    "selector": "source.python",
-    "file_regex": "^\\s*File \"(...*?)\", line ([0-9]*)"
-}
-
-fazendo as alterações necessárias de acordo com o seu
-diretorio virtualenv
-
-OBjetivo deste APP:
-Interface GUI
-Exibir as opções disponíveis para download
-Setar uma configuração padrao para download. p.ex.360p
-
-https://pypi.org/project/pytube/
-
-Streams onde progressive = true tem o audio e o video no mesmo arquivo
-"""
+# https://stackoverflow.com/questions/55735775/how-do-i-add-a-progress-bar-to-my-pytube-code
 
 def cria_lista_de_opcoes(video_obj):
   yt_stream_list = video_obj.streams.filter(progressive=True)
@@ -61,21 +29,33 @@ def exception_routine(error):
   print("Erro: ", str(error))
   raise SystemExit
 
+
+def main():
+	stream_options = cria_lista_de_opcoes(video_obj)
+	show_user_option(stream_options)
+	user_choice_itag = get_user_option(stream_options)
+	selected_stream = video_obj.streams.get_by_itag(user_choice_itag)
+	selected_stream.download()
+	print("Encerrado com sucesso")
+
+
+def progress_func(stream, chunk, bytes_remaining):
+	print('on progress')
+	curr = stream.filesize - bytes_remaining
+	done = int(50 * curr / stream.filesize)
+	sys.stdout.write("\r[{}{}] ".format('=' * done, ' ' * (50-done)) )
+	sys.stdout.flush()	
+
+def complete_func(stream, file_path):
+	print('on complete callback')
+
 from pytube import YouTube
 DOWNLOAD_FOLDER = "./downloads"
 # Israel com Aline
 # QUAIS OS NOMES MAIS POPULARES EM ISRAEL? Atualizado 2022
 video_url = "https://www.youtube.com/watch?v=U6brR0LGo8A"
-video_obj = YouTube(video_url)
-#stream = video_obj.streams.get_highest_resolution()
-#stream.download(DOWNLOAD_FOLDER)
-
-stream_options = cria_lista_de_opcoes(video_obj)
-show_user_option(stream_options)
-user_choice_itag = get_user_option(stream_options)
-selected_stream = video_obj.streams.get_by_itag(user_choice_itag)
-selected_stream.download()
-print("Encerrado com sucesso")
-
-
-
+video_obj = YouTube(video_url, on_progress_callback = progress_func,
+			on_complete_callback=complete_func)
+stream = video_obj.streams.get_by_itag(17)
+stream.download(DOWNLOAD_FOLDER)
+print('End.')
