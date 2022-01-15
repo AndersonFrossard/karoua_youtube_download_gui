1f079dac83f8c19ca09a26d42e4de755dbba8b56
AndersonFrossard <frossard2008@gmail.com>
2022-01-15 16:31:28 -0300

    Added Download progress percentage info

--- youtube_download.py
+++ youtube_download.py
@@ -1,5 +1,6 @@
 # https://stackoverflow.com/questions/55735775/how-do-i-add-a-progress-bar-to-my-pytube-code
 
+
 def cria_lista_de_opcoes(video_obj):
   yt_stream_list = video_obj.streams.filter(progressive=True)
   stream_options =[]
@@ -38,24 +39,28 @@ def main():
 	selected_stream.download()
 	print("Encerrado com sucesso")
 
+def on_progress_func(stream, chunk, bytes_remaining):
+	total_size = stream.filesize
+	bytes_downloaded = total_size - bytes_remaining
+	percentage_of_completion = int (bytes_downloaded / total_size * 100)
+	print(percentage_of_completion,' % Completed')
 
-def progress_func(stream, chunk, bytes_remaining):
-	print('on progress')
-	curr = stream.filesize - bytes_remaining
-	done = int(50 * curr / stream.filesize)
-	sys.stdout.write("\r[{}{}] ".format('=' * done, ' ' * (50-done)) )
-	sys.stdout.flush()	
-
-def complete_func(stream, file_path):
-	print('on complete callback')
+def on_complete_func(stream, file_path):
+	print('Download complete')
 
 from pytube import YouTube
+import sys
 DOWNLOAD_FOLDER = "./downloads"
 # Israel com Aline
 # QUAIS OS NOMES MAIS POPULARES EM ISRAEL? Atualizado 2022
 video_url = "https://www.youtube.com/watch?v=U6brR0LGo8A"
-video_obj = YouTube(video_url, on_progress_callback = progress_func,
-			on_complete_callback=complete_func)
-stream = video_obj.streams.get_by_itag(17)
+video_obj = YouTube(video_url,
+			on_progress_callback = on_progress_func,
+			on_complete_callback = on_complete_func )
+
+stream_options = cria_lista_de_opcoes(video_obj)
+show_user_option(stream_options)
+user_choice_itag = get_user_option(stream_options)
+stream = video_obj.streams.get_by_itag(user_choice_itag)
 stream.download(DOWNLOAD_FOLDER)
 print('End.')
