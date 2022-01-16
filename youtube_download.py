16a5c7a61ff967fea16f33b3a0b8021f7cf955b9
AndersonFrossard <frossard2008@gmail.com>
2022-01-16 12:38:02 -0300

    Added exception handling.

--- youtube_download.py
+++ youtube_download.py
@@ -1,12 +1,17 @@
 # https://stackoverflow.com/questions/55735775/how-do-i-add-a-progress-bar-to-my-pytube-code
-
+#Use this option to run under google colab:
+#from pytube import exceptions 
 
 def cria_lista_de_opcoes(video_obj):
-  yt_stream_list = video_obj.streams.filter(progressive=True)
-  stream_options =[]
-  for item in yt_stream_list:
-    stream_options.append([item.itag, item.resolution])
-  return stream_options
+  try:
+    yt_stream_list = video_obj.streams.filter(progressive=True)
+    stream_options =[]
+    for item in yt_stream_list:
+      stream_options.append([item.itag, item.resolution])
+    return stream_options
+  except Exception as e:
+    exception_routine(e)
+    
 
 def show_user_option(stream_options):
   i=0
@@ -26,10 +31,31 @@ def get_user_option(stream_options):
     exception_routine(error)
 
 def exception_routine(error):
-  print("Opção inválida !!!")
-  print("Erro: ", str(error))
+  if type(error) == IndexError:
+    print('Opção inválida !! ')
+  elif type(error) == ValueError:
+    print('Opção inválida. Somente números são permitidos.')
+  elif type(error) == pytube.exceptions.VideoUnavailable:
+    print('Erro: Vídeo não disponível.')
+    print('Você entrou com a URL correta? Utilizar conforme o exemplo :')
+    print('https://www.youtube.com/watch?v=NNNNNNNNNNN')
+  elif type(error) == pytube.exceptions.VideoPrivate:
+      print('Erro: Vídeo é privado. Download não disponível.')
+  elif type(error) == pytube.exceptions.LiveStreamError:
+      print('Erro: Vídeo é uma live. Download não disponível.')
+  elif type(error) == pytube.exceptions.AgeRestrictedError:
+      print('Video is age restricted, and cannot be accessed without OAuth.')
+  elif type(error) == pytube.exceptions.MaxRetriesExceeded:
+      print('Maximum number of retries exceeded.')
+  elif type(error) == pytube.exceptions.MembersOnly:
+      print('Error: Video is members-only.')
+      print('YouTube has special videos that  are only viewable to users who')
+      print('have subscribed to a content creator')
+  elif type(error) == pytube.exceptions.VideoRegionBlocked:
+      print('Error: Video is blocked to your region. Try using a VPN.')
+  else:
+    print(str(error))
   raise SystemExit
-
 
 def main():
 	stream_options = cria_lista_de_opcoes(video_obj)
@@ -43,17 +69,30 @@ def on_progress_func(stream, chunk, bytes_remaining):
 	total_size = stream.filesize
 	bytes_downloaded = total_size - bytes_remaining
 	percentage_of_completion = int (bytes_downloaded / total_size * 100)
-	print(percentage_of_completion,' % Completed')
+	print("\r Downloading ...... %d%%" % percentage_of_completion,end="")
 
 def on_complete_func(stream, file_path):
-	print('Download complete')
+	print('\nDownload complete')
 
+#/-==============================================================
+#/-
+#/-  Here begins the main module
+#/-
+#/-==============================================================
 from pytube import YouTube
+import pytube.exceptions 
 import sys
+
+video_url = "https://www.youtube.com/watch?v=U6brR0LGo8A"; noise="asdasdads"
+video_obj = YouTube(video_url)
+
+
 DOWNLOAD_FOLDER = "./downloads"
 # Israel com Aline
 # QUAIS OS NOMES MAIS POPULARES EM ISRAEL? Atualizado 2022
 video_url = "https://www.youtube.com/watch?v=U6brR0LGo8A"
+#video_url = "htasdasdastps://www.youtadasdasdsaube.com/watch?v=U6brasdasdasR0LGo8A"
+
 video_obj = YouTube(video_url,
 			on_progress_callback = on_progress_func,
 			on_complete_callback = on_complete_func )
